fb4c8d033df32b1342097c49dfa03296d7992510
1eejaehyeon <90614462+1eejaehyeon@users.noreply.github.com>
2022-01-15 01:09:45 +0900

    최종수정 해봄

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,5 +126,5 @@ STATICFILES_DIRS = (BASE_DIR / 'static',)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+STATIC_ROOT = '/static/'  # os.path.join(BASE_DIR, 'static')
